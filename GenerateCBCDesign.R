3d00128ecc0439e72d86b187296b2c98a26e37b1
crforsythe <82175707+crforsythe@users.noreply.github.com>
2022-04-13 10:07:40 -0400

    Fixed spelling

--- GenerateCBCDesign.R
+++ GenerateCBCDesign.R
@@ -5,7 +5,7 @@ set.seed(1995912)
 levelValues <- list(
   price = c("$0", "$5", "$10", "$15", "$20", "$25", "$30"),
   time = c("Today", 'Tomorrow', 'In 4 days', 'In 7 days'),
-  agent = c('Human', 'Autnomous vehicle'),
+  agent = c('Human', 'Autonomous vehicle'),
   location = c('Doorstep', 'Curbside', 'Neighborhood locker')
 )
 
